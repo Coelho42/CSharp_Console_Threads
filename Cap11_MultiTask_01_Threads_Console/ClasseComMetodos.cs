d17491985f293e0de3ad53a205a99601c43610d2
Goncalo Miguel Martins Coelho <newmaximus0@gmail.com>
2018-12-21 17:40:34 +0000

    - Projeto com programação Asyncrona

---
+++ Cap11_MultiTask_01_Threads_Console/ClasseComMetodos.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cap11_MultiTask_01_Threads_Console
+{
+    /// <summary>
+    /// Esta classe possuí 2 métodos
+    /// - DoLongRunningStuffSync(): Simula um longo processamento em modo Síncrono
+    /// - DoLongRunningStuffAsync(): Simula um longo processamento em modo Assíncrono
+    /// O retorno de um método assíncrono
+    /// </summary>
+    class ClasseComMetodos
+    {
+        /// <summary>
+        /// Simula um longo processamento em modo Síncrono
+        /// </summary>
+        public void DoLongRunningStuffSync()
+        {
+            int counter;
+            for (counter = 0; counter < 50000; counter++)
+            {
+                Console.WriteLine(counter);
+            }
+            // return "Counter = " + counter;
+        }
+
+        /// <summary>
+        /// Simula um longo processamento em modo Assíncrono
+        /// </summary>
+        /// <returns></returns>
+        public async Task DoLongRunningStuffAsync()
+        {
+            await Task.Run(() =>
+            {
+                int counter;
+                for (counter = 0; counter < 50000; counter++)
+                {
+                    Console.WriteLine(counter);
+                }
+            });
+        }
+    }
+}
--- Cap11_MultiTask_01_Threads_Console/Program.cs
+++ Cap11_MultiTask_01_Threads_Console/Program.cs
@@ -6,16 +6,63 @@ using System.Threading.Tasks;
 
 namespace Cap11_MultiTask_01_Threads_Console
 {
+    /// <summary>
+    /// Projeto de demonstração simples de multitarefa
+    /// A Main do programa corre 2 ciclos.
+    /// - Um a partir de um método sincrono ou assincrono 
+    /// - Outro no final da main
+    /// Em circunstâncias normais, executa 1º um ciclo e só depois o 2º.
+    /// O método sincrono faz exatamente isso. É sequencial.
+    /// Mas o método assíncrono coloca o 1º ciclo numa thread, o que resulta na execução simultânea com o 2º ciclo.
+    /// Para efeitos de teste, os métodos síncrono e assíncrono estão duplicados:
+    /// - Na própria Classe Program como static e private
+    /// - Na ClasseComMetodos todos como public.
+    /// </summary>
     class Program
     {
         static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            // Executar apenas um dos métodos Locais ou da Classe:
+            // DoLongRunningStuffSync();   // Método síncrono
+            // DoLongRunningStuffAsync();   // Método Assíncrono
 
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+            ClasseComMetodos demo = new ClasseComMetodos();
+            // demo.DoLongRunningStuffSync();    // Método síncrono
+            demo.DoLongRunningStuffAsync();      // Método Assíncrono
+
+            // Ciclo infinito. Quando é que é executado?
+            while(true)
+            {
+                Console.WriteLine("A fazer coisas na Thread Principal ...............................");
+            }
+        }
+
+        /// <summary>
+        /// Simula um longo processamento em modo Síncrono
+        /// </summary>
+        static void DoLongRunningStuffSync()
+        {
+            int counter;
+            for (counter = 0; counter < 50000; counter++)
+            {
+                Console.WriteLine(counter);
+            }
+        }
+
+        /// <summary>
+        /// Simula um longo processamento em modo Assíncrono
+        /// </summary>
+        /// <returns></returns>
+        static async Task DoLongRunningStuffAsync()
+        {
+            await Task.Run(() =>
+            {
+                int counter;
+                for (counter = 0; counter < 50000; counter++)
+                {
+                    Console.WriteLine(counter);
+                }
+            });
         }
     }
 }
